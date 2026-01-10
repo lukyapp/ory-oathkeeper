f6ac1542a099f3adad566926627dc077ad460e59
llucas.spot <llucas.spot@gmail.com>
2026-01-10 20:08:37 +0100

    fix oathkeeper 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -eu
 : "${HYDRA_ADMIN_URL:?Missing HYDRA_ADMIN_URL}"
 : "${HYDRA_CLIENT_ID:?Missing HYDRA_CLIENT_ID}"
 : "${HYDRA_CLIENT_SECRET:?Missing HYDRA_CLIENT_SECRET}"
+: "${KETO_READ_URL:?Missing KETO_READ_URL}"
 
 envsubst < /etc/oathkeeper/oathkeeper.yml.tmpl > /etc/oathkeeper/oathkeeper.yml
 
